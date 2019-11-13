ae52b5751504af11ffe39e07397019e95269ece1
LukeKrishna <57178543+LukeKrishna@users.noreply.github.com>
2019-11-13 11:33:15 +0100

    Shows the list of drugs available

--- listOfKnownDrugs.js
+++ listOfKnownDrugs.js
@@ -28,13 +28,17 @@ async function main () {
   document.getElementById('output').innerHTML =              //displays the result on the page
     JSON.stringify(simpleResults, undefined, 2);             //simplifys JSON to not display uri, etc.
   */
-let drugList = '';
+let drugList = '<option value="none">';
+let counter1 = 0;
 //  let drugUser = drugSubClean;                               
   
   for (i=0; i < simpleResults.length; i++){
     drugList += `<option value="${simpleResults[i].drug.label}">`;
-    console.log(simpleResults[i].drug.label);
+    counter1++;
+    // console.log(simpleResults[i].drug.label);
   }
+
+console.log(counter1);
 
 document.getElementById('drug').innerHTML = drugList;
 }
