7b6fdc381033d8d8cea5194d083e3bbd82ea9b8c
LukeKrishna <lukas.p.filipcik@gmail.com>
2019-11-23 17:20:38 +0100

    Promises resolved: made into normal Array

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
  // This is the 'lynchpin'  file for doing the computations
 let valArray = getChoiceArray();
 
-var input = getID(queryForID('P592'));
-	input.then(function(result){
-	// console.log(result);
-});
+// var input = getID(queryForID('P592'));
+// 	input.then(function(result){
+// 	// console.log(result);
+// });
+
+// let resLD = queryForID(idIdentifier);
+// 	resLD.then(function(result){
+// 		// console.log(result)
+// });
+
+// async function test1(){
+// 	// let finalTest = resLD
+// 	let idIdentifier = await input;
+
+// 	let resLD = queryForLD(idIdentifier);
+// 	resLD.then(function(result){
+// 		// console.log(result)
+// 	});
+
+// 	let finalTest = await resLD;
+// 	console.log(finalTest);
+// }
+
+// test1();
 
 // console.log(getID(queryForID('P592')));
 // console.log(getID(queryForID('P662')));
@@ -13,16 +33,37 @@ var input = getID(queryForID('P592'));
 
 // console.log(test);
 
-function createFinalOutput(idIdentifier){
+async function createFinalOutput(idIdentifier){
+	
+	
+	let resLD = queryForLD(idIdentifier);
+		resLD.then(function(result){});
+
+	let resChemStr = queryForChemicalStructure(idIdentifier);
+		resChemStr.then(function(result){});
+
+	let resPrimPha = queryForPrimePharm(idIdentifier);
+		resPrimPha.then(function(result){});
+
+	let resDrugInt = queryForDrugInteraction(idIdentifier);
+		resDrugInt.then(function(result){});
+	
+	let resArticle = queryForArticles(idIdentifier);
+		resArticle.then(function(result){});
+	
+	let resPregCat = queryForPregnancyCategory(idIdentifier);
+		resPregCat.then(function(result){});
+
+
 	let finalArray = [0]; 
 
 	finalArray = [ 
-						(valArray[0]) ? queryForLD(idIdentifier): 0, 
-						(valArray[1]) ? queryForChemicalStructure(idIdentifier): 0,
-						(valArray[2]) ? queryForPrimePharm(idIdentifier): 0,
-						(valArray[3]) ? queryForDrugInteraction(idIdentifier): 0,
-						(valArray[4]) ? queryForArticles(idIdentifier): 0,
-						(valArray[5]) ? queryForPregnancyCategory(idIdentifier): 0,
+						(valArray[0]) ? await resLD: 0, 
+						(valArray[1]) ? await resChemStr: 0,
+						(valArray[2]) ? await resPrimPha: 0,
+						(valArray[3]) ? await resDrugInt: 0,
+						(valArray[4]) ? await resArticle: 0,
+						(valArray[5]) ? await resPregCat: 0,
 				 ];
 
 
@@ -33,11 +74,20 @@ function createFinalOutput(idIdentifier){
 
 // let resultArray = JSON.stringify(createFinalOutput(await.input)); 
 
-async function main() {
+async function finaliseResultsFromPromises() {
 	// body...
-   let resultArray = createFinalOutput(await input); 
+   var input = getID(queryForID('P592'));
+	input.then(function(result){
+	// console.log(result);
+   });
 
-   console.log(resultArray);
+   // let resultArray = createFinalOutput(await input); 
+
+   let finArr = createFinalOutput(await input);
+   	   finArr.then(function(result){});
+
+   // console.log(resultArray);
+   console.log(await finArr);
 }
 
-main();
+finaliseResultsFromPromises();
